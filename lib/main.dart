f6a0e8d98a7bb51dcd6b7b28544e281ac06d8c7e
AbdullahChauhan <acse.uok@gmail.com>
2020-03-19 20:29:22 +0500

    adjusting imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:weather_app/screens/dashboard.dart';
-import 'package:weather_app/services/api.dart';
+import 'package:weather_app/app/screens/dashboard.dart';
+import 'package:weather_app/app/services/api.dart';
 import 'package:weather_app/services/api_service.dart';
 
 void main() {
