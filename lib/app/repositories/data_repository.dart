5928f007a7f7f1722437b8c5e76f5698d6749419
AbdullahChauhan <acse.uok@gmail.com>
2020-03-23 15:51:41 +0500

    added data-repository for business logics

---
+++ lib/app/repositories/data_repository.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart';
+import 'package:weather_app/app/services/api_service.dart';
+
+class DataRepository {
+  final APIService apiService;
+
+  DataRepository({@required this.apiService});
+
+}
